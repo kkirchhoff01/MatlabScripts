b40f134355ade4f43c38b8dd61568efbe3e03956
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2015-12-01 11:00:23 -0600

    fixed and simplified

--- RungeKutta.m
+++ RungeKutta.m
@@ -1,11 +1,8 @@
 function [y,e] = RungaKutta(h, y0, t0, f, Y)
-	y(1) = y0;
-	t(1) = t0;
-	e(1) = Y(0) - y(1);
-	for j = 1:(1/h)+1
-		t(j) = t0 + (j-1)*h;
+	y(1) = y0 + h*f(t0 + (h/2), y0 + (h/2)*f(t0,y0));
+	for j = 1:(1/h)
+		t(j) = t0 + j*h;
 		y(j+1) = y(j) + h*f(t(j) + (h/2), y(j) + (h/2)*f(t(j),y(j)));
 		e(j) = Y(t(j)) - y(j);
 	end
-	e((1/h)+2) = Y(t0+h*((1/h)+2)) - y((1/h)+2);
 end
